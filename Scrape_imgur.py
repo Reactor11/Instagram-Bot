5c340c75bec580c3c95d6b9aae88fcf387e71a14
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2020-04-27 11:14:54 +0530

    Update Scrape_imgur.py

--- Scrape_imgur.py
+++ Scrape_imgur.py
@@ -6,14 +6,14 @@ import time
 from lxml.html import fromstring
 import numpy as np
 import re
+from bs4 import BeautifulSoup
 import logging 
+from datetime import datetime
 import urllib.request
-
 logging.basicConfig(filename="scrapeImgur.log", format='%(levelname)s  %(message)s',filemode='a')  
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG) 
-logger.info("---------------------------------STARTED EXECUTION---------------------------------")
-
+logger.info("---------------------------------STARTED EXECUTION AT {}---------------------------------".format(datetime.now()))
 def get_proxies():
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
@@ -25,7 +25,6 @@ def get_proxies():
             proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
     return list(proxies)
-
 url = "https://imgur.com/gallery"
 if(sys.platform == 'linux'):
     webDriveLoc = os.path.abspath(os.getcwd()) + "/chromedriver"
@@ -34,38 +33,34 @@ else:
     webDriveLoc = os.path.abspath(os.getcwd()) + "\\chromedriver.exe"
     logger.info("Linux operating system, chromeDriver location : " + webDriveLoc)
 PROXY = get_proxies()
-
 chrome_option = webdriver.ChromeOptions()
 chrome_option.add_argument("window-size=1920,1080")
 chrome_option.add_argument("--incognito")
 # if(len(PROXY)!=0):
 #     chrome_option.add_argument('--proxy-server=%s' % PROXY[np.random.randint(0,len(PROXY))])
-# chrome_option.add_argument("--headless")
-
+chrome_option.add_argument("--headless")
 logger.info("Setting up Chrome options : " + " ".join(chrome_option.arguments))
-
 driver = webdriver.Chrome(executable_path=webDriveLoc,options=chrome_option)
 logger.info(driver)
 driver.get(url)
-# time.sleep(5)
+time.sleep(10)
 # driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 # time.sleep(5)
-
 linksList = set()
 for tag in driver.find_elements_by_tag_name('a'):
     x = re.findall("https://imgur.com/gallery/[a-zA-z0-9]*[^/]",tag.get_attribute('href'))
     if(len(x)!=0):
         linksList.add(x[0])
 linksList = list(linksList)
+print(len(linksList))
 # driver.quit()
-
 # linksList = ['https://imgur.com/gallery/zY4fAAF', 'https://imgur.com/gallery/iZqLakX','https://imgur.com/gallery/wIXX8S6']
 imageLinksList = list()
 videoLinksList = list()
-for link in linksList:
+for link in linksList[:10]:
     try:
         driver.get(str(link))
-        time.sleep(3)
+        time.sleep(5)
         for tag in driver.find_elements_by_class_name('image-placeholder'):
             imageLinksList.append(tag.get_attribute('src'))
             logger.info("Image link : " + tag.get_attribute('src'))
@@ -76,9 +71,17 @@ for link in linksList:
     except :
         logger.error("Cannot parse the link : " + link)
 #         logger.error("Exception caught is : " )
-
+count = 0
 for link in imageLinksList:
-    urllib.request.urlretrieve(link, 'images/' + link.split('/')[-1])
-    logger.info("Image saved to : images/" + link.split('/')[-1])
+    try:
+        imageName = link.split('/')[-1]
+        if(imageName not in os.listdir('images')):
+            urllib.request.urlretrieve(link, 'images/' + imageName)
+            logger.info("Image saved to : images/" + imageName)
+            count = count+1
+    except:
+        logger.error("Cannot save the image link : " + link)
+logger.info("Saved " + str(count) + " Images.")
 # for link in videoLinksList:
 #     urllib.request.urlretrieve(link, 'videos/' + link.split('/')[-1])
+driver.quit()
