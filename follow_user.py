14e42da88dd3b6d326b4c479dd6934c5c3f454e3
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2020-05-03 01:00:10 +0530

    Add files via upload

---
+++ follow_user.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from instabot import Bot
+
+bot = Bot(filter_users=False)
+users_to_follow = bot.read_list_from_file('followers.txt')
+if not users_to_follow:
+    exit()
+else:
+    print("Found %d users in file." % len(users_to_follow))
+
+bot.login(username="trollcharge", password="instagram1!")
+
+bot.follow_users(users_to_follow)
